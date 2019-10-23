f0af6aa379425e7c2e40bfafbf359d3b77d4e164
Robert Evans <robertevans@robertevansmb.com>
2019-10-23 16:08:06 -0400

    Add Crisis List Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent
   ],
   imports: [
     BrowserModule,
